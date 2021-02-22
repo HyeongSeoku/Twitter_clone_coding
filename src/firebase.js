ed74d408188746559c7dc20c7e6c43d124f52920
gudtjr3437 <gudtjr3437@naver.com>
2021-02-23 00:38:54 +0900

    #1.0 React & Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAbH7qQqWxtw6t314rQDaG98mWGv2KnpWE",
+    authDomain: "nwitter-ad3ae.firebaseapp.com",
+    projectId: "nwitter-ad3ae",
+    storageBucket: "nwitter-ad3ae.appspot.com",
+    messagingSenderId: "771392418483",
+    appId: "1:771392418483:web:b1a49f62e4b072cbf576c6"
+  };
+
+ export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import firebase from "./firebase";
+
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root") //'root' 로 돼있으면 css 오류남 "root"로 변경
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
