815a1206100e6af5bd933005c053a9103059f1f7
gudtjr3437 <gudtjr3437@naver.com>
2021-02-24 17:53:52 +0900

    Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {HashRouter as Router, Route,Switch} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home"
+
+const AppRouter = () => {
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
+    return(
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                    <>
+                <Router exact path ="/">
+                    <Home />
+                </Router> 
+                </>     //Fragment 묶고싶을때 사용 div 등을 사용하고 싶지 않고 그냥 묶고만 싶을때
+                ):( 
+                 <>
+                <Router exact path ="/">
+                    <Auth />
+                </Router>
+                </>
+                )}
+            </Switch>
+        </Router>
+    );
+}
+export default AppRouter;
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAbH7qQqWxtw6t314rQDaG98mWGv2KnpWE",
-    authDomain: "nwitter-ad3ae.firebaseapp.com",
-    projectId: "nwitter-ad3ae",
-    storageBucket: "nwitter-ad3ae.appspot.com",
-    messagingSenderId: "771392418483",
-    appId: "1:771392418483:web:b1a49f62e4b072cbf576c6"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
  export default firebase.initializeApp(firebaseConfig);
