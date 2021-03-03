e78a7d4b471772db042cce950bd352c4e0823c74
gudtjr3437 <gudtjr3437@naver.com>
2021-03-03 12:08:27 +0900

    2.4 Log in part2

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,38 +2,67 @@ import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [newAccount,setNewAccount] = useState(true);
-    const onChange = (event) => {
-        const {target:{name,value}}=event;
-        if(name === "email"){
-            setEmail(value);
-        }else if (name === "password"){
-            setPassword(value);
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
     }
-    const onSubmit = async (event) =>{
-        event.preventDefault();     //preventDefault = 기본 행위 방지 
-        let data;
-        try{
-            if(newAccount){
-                data = await authService.createUserWithEmailAndPassword(email,password);
-            }else{
-                data = await authService.signInWithEmailAndPassword(email,password);
-            }
-            console.log(data);
-        }catch(error){
-            console.log(error);
-        }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault(); //preventDefault = 기본 행위 방지
+    let data;
+    try {
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
     }
+  };
+
+  const toggleAccount = () =>setNewAccount ((prev) => !prev);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} name="email" type="email" placeholder="Email" required value={email} />
-        <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password} />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} required />
+        <input
+          onChange={onChange}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+        />
+        <input
+          onChange={onChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+          required
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In": "Create Account"}</span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
